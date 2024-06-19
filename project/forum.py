f0f18041a74c92cd1ac241d1393fc85cc9720f27
Paweł Harasiuk <pharasiuk01@gmail.com>
2024-06-19 13:15:54 +0200

    created forum

---
+++ project/forum.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask_login import login_required, current_user
+from .models import Thread, Post
+from . import db
+
+forum = Blueprint('forum', __name__)
+
+@forum.route('/threads', methods=['GET', 'POST'])
+@login_required
+def threads():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        new_thread = Thread(title=title, user_id=current_user.id)
+        db.session.add(new_thread)
+        db.session.commit()
+        return redirect(url_for('forum.threads'))
+    threads = Thread.query.order_by(Thread.timestamp.desc()).all()
+    return render_template('threads.html', threads=threads)
+
+@forum.route('/thread/<int:id>', methods=['GET', 'POST'])
+@login_required
+def thread(id):
+    thread = Thread.query.get_or_404(id)
+    if request.method == 'POST':
+        content = request.form.get('content')
+        new_post = Post(content=content, user_id=current_user.id, thread_id=thread.id)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('forum.thread', id=id))
+    posts = Post.query.filter_by(thread_id=thread.id).order_by(Post.timestamp.desc()).all()
+    return render_template('thread.html', thread=thread, posts=posts)
+
+@forum.route('/upvote/<int:id>')
+@login_required
+def upvote(id):
+    post = Post.query.get_or_404(id)
+    post.upvotes += 1
+    db.session.commit()
+    return jsonify(success=True, count=post.upvotes)
+
+@forum.route('/downvote/<int:id>')
+@login_required
+def downvote(id):
+    post = Post.query.get_or_404(id)
+    post.downvotes += 1
+    db.session.commit()
+    return jsonify(success=True, count=post.downvotes)
---
+++ project/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from . import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
+class Thread(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    upvotes = db.Column(db.Integer, default=0)
+    downvotes = db.Column(db.Integer, default=0)
+    posts = db.relationship('Post', backref='thread', lazy='dynamic')
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    upvotes = db.Column(db.Integer, default=0)
+    downvotes = db.Column(db.Integer, default=0)
